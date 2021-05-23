74d557c5876051a05e40ca40502711bbd8e8779e
AbdurahimAqlli <60809325+AbdurahimAqlli@users.noreply.github.com>
2021-05-23 08:39:05 +0500

    added menus

--- src/main/java/uz/asz/myapp/telegram/Telegram.java
+++ src/main/java/uz/asz/myapp/telegram/Telegram.java
@@ -1,25 +1,26 @@
 package uz.asz.myapp.telegram;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendLocation;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardRemove;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import uz.asz.myapp.domain.Group;
 import uz.asz.myapp.domain.Student;
-import uz.asz.myapp.domain.enumeration.Category;
 
-//@Component todo shu annotationni yoqsam telegram bot ishlami qovotti agar yoqmasam  CheckUserService ,
-// todo RegisterUserService servicelarni  chaqiromayamman init bo`mayabdi null bo`lib qolib ketvotti
+@Component
 public class Telegram extends TelegramLongPollingBot {
 
     private static final String BOT_USERNAME = "abdurahimasz_bot";
@@ -44,22 +45,27 @@ public class Telegram extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         Message message = update.getMessage();
-        SendMessage sendMessage = new SendMessage();
         try {
             if (checkUserService.checkTeacherInDb(message.getChatId())) {
+                SendMessage sendMessage = new SendMessage();
                 sendMessage.setText("salom ustoz che gap");
                 sendMessage.setChatId(message.getChatId());
                 execute(sendMessage);
+                return;
             }
             if (checkUserService.checkInstructorInDb(message.getChatId())) {
+                SendMessage sendMessage = new SendMessage();
                 sendMessage.setText("salom instruktor che gap");
                 sendMessage.setChatId(message.getChatId());
                 execute(sendMessage);
+                return;
             }
             if (checkUserService.checkStudentInDb(message.getChatId())) {
+                SendMessage sendMessage = new SendMessage();
                 sendMessage.setText("salom student che gap");
                 sendMessage.setChatId(message.getChatId());
                 execute(sendMessage);
+                return;
             }
             if (stepEnums == null) {
                 stepEnums = StepEnums.BEGIN;
@@ -72,6 +78,7 @@ public class Telegram extends TelegramLongPollingBot {
 
             Student student = new Student();
             if (Objects.equals(message.getText(), "\uD83D\uDCDD Отправить заявку на обучение")) {
+                SendMessage sendMessage = new SendMessage();
                 stepEnums = StepEnums.REGISTER_FOR_TEACHING_1;
                 sendMessage.setChatId(message.getChatId());
                 sendMessage.setText("✅ Отправьте нам Ф.И.О.");
@@ -79,8 +86,92 @@ public class Telegram extends TelegramLongPollingBot {
                 execute(sendMessage);
                 return;
             }
+            if (Objects.equals(message.getText(), "\uD83D\uDCF0 Необходимые документы")) {
+                SendMessage sendMessage = new SendMessage();
+                sendMessage.setText(
+                    "\uD83D\uDCC7 Копия паспорта с пропиской\n" +
+                    "\uD83D\uDCC4 Медицинская справка формы 083\n" +
+                    "\uD83D\uDCD6 Копия атестата либо диплома\n" +
+                    "\uD83D\uDC68\u200D\uD83D\uDCBC 4 фото (размер 3х4)"
+                );
+                sendMessage.setChatId(message.getChatId());
+                execute(sendMessage);
+                return;
+            }
+            if (Objects.equals(message.getText(), "\uD83D\uDCB0Цена")) {
+                SendMessage sendMessage = new SendMessage();
+                sendMessage.setText(
+                    "\uD83D\uDCB0 Цена курса 1 смены (с 9:00 до 12:00)\n" +
+                    "                     равна 1 600 000 сум.\n" +
+                    "------------------------------------------------\n" +
+                    "\uD83D\uDCB0 Цена курса 2 смены (c 12:00 до 15:00)\n" +
+                    "                     равна 1 600 000 сум.\n" +
+                    "------------------------------------------------\n" +
+                    "\uD83D\uDCB0 Цена курса 3 смены (c 15:00 до 18:00)\n" +
+                    "                     равна 1 600 000 сум.\n" +
+                    "------------------------------------------------\n" +
+                    "\uD83D\uDCB0 Цена курса 4 смены (c 18:00 до 21:00) \n" +
+                    "                     равна 1 700 000 сум.\n" +
+                    "------------------------------------------------\n" +
+                    "❗️В стоимость включены все расходы связанные с обучением и НИКАКИХ СКРЫТЫХ ДОПЛАТ❗️\n" +
+                    "\uD83D\uDCC6 При желании можно оплачивать частями. Первый взнос 500 000\n" +
+                    "\uD83D\uDCB3 Форма оплаты любая (терминал, наличные, перечисление"
+                );
+                sendMessage.setChatId(message.getChatId());
+                execute(sendMessage);
+                return;
+            }
+            if (Objects.equals(message.getText(), "⏰ График занятий")) {
+                SendMessage sendMessage = new SendMessage();
+                sendMessage.setText(
+                    "\uD83D\uDD58 Смена #1 (с 9:00 до 12:00)\n" +
+                    "\uD83D\uDD5B Смена #2 (с 12:00 до 15:00)\n" +
+                    "\uD83D\uDD52 Смена #3 (с 15:00 до 18:00)\n" +
+                    "\uD83D\uDD61 Смена #4 (с 18:00 до 21:00)\n" +
+                    "\uD83D\uDCC6 Курс длится 56 дней (2,5 месяца).\n" +
+                    "\uD83D\uDCDD Теоретические занятия проводятся в онлайн режиме 5 дней в неделю с понедельника по пятницу.\n" +
+                    "\uD83D\uDE98 Практические занятия по вождению проводятся 3 дня в неделю в соответствии с графиком."
+                );
+                sendMessage.setChatId(message.getChatId());
+                execute(sendMessage);
+                return;
+            }
+            if (Objects.equals(message.getText(), "☎️ Наши контакты")) {
+                SendMessage sendMessage = new SendMessage();
+                sendMessage.setText(
+                    "☎️ Тел.: 93 575 80 30\n" +
+                    "\uD83D\uDCF1 Telegram: t.me/intercruz\n" +
+                    "\uD83D\uDEC2 Telegram bot: t.me/intercruzbot\n" +
+                    "\uD83C\uDF10 Сайт: www.intercruz.uz \n" +
+                    "\uD83D\uDCE7 Еmail: support@intercruz.uz\n" +
+                    "\uD83D\uDCF7 Instagram: instagram.com/intercruz.uz\n" +
+                    "\uD83D\uDCBB Facebook: fb.com/driving.school.Intercruz/"
+                );
+                sendMessage.setChatId(message.getChatId());
+                execute(sendMessage);
+                return;
+            }
+            if (Objects.equals(message.getText(), "\uD83D\uDCF7  Фотогалерея")) {
+                SendPhoto sendMessage = new SendPhoto()
+                    .setPhoto(
+                        "my angel",
+                        new FileInputStream(new File("C:\\Users\\Lenovo\\Desktop\\salom\\shaxsiy\\200095900609_184873.jpg"))
+                    );
+                sendMessage.setCaption("my angel");
+                sendMessage.setChatId(message.getChatId());
+                execute(sendMessage);
+                return;
+            }
+            if (Objects.equals(message.getText(), "\uD83D\uDCCDНаш адрес")) {
+                SendLocation sendLocation = new SendLocation().setLatitude(41.383346F);
+                sendLocation.setLongitude(69.2857343F);
+                sendLocation.setChatId(message.getChatId());
+                execute(sendLocation);
+                return;
+            }
             if (stepEnums == StepEnums.REGISTER_FOR_TEACHING_1) {
                 stepEnums = StepEnums.REGISTER_FOR_TEACHING_2;
+                SendMessage sendMessage = new SendMessage();
                 sendMessage.setChatId(message.getChatId());
                 student.setFirstName(message.getForwardSenderName());
                 student.setLastName(message.getForwardSenderName());
@@ -92,6 +183,7 @@ public class Telegram extends TelegramLongPollingBot {
             }
             if (stepEnums == StepEnums.REGISTER_FOR_TEACHING_2) {
                 stepEnums = StepEnums.REGISTER_FOR_TEACHING_3;
+                SendMessage sendMessage = new SendMessage();
                 sendMessage.setChatId(message.getChatId());
                 sendMessage.setText("✅ В какой смене вы хотели бы обучаться?");
                 student.setPhoneNumber(message.getText());
@@ -100,6 +192,7 @@ public class Telegram extends TelegramLongPollingBot {
             }
             if (stepEnums == StepEnums.REGISTER_FOR_TEACHING_3) {
                 stepEnums = StepEnums.REGISTER_FOR_TEACHING_4;
+                SendMessage sendMessage = new SendMessage();
                 sendMessage.setChatId(message.getChatId());
                 sendMessage.setText("Qaysi category bo`yicha o`qimoqchisz!");
                 execute(sendMessage);
@@ -107,6 +200,7 @@ public class Telegram extends TelegramLongPollingBot {
             }
             if (stepEnums == StepEnums.REGISTER_FOR_TEACHING_4) {
                 stepEnums = StepEnums.REGISTER_FOR_TEACHING_5;
+                SendMessage sendMessage = new SendMessage();
                 sendMessage.setChatId(message.getChatId());
                 ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
                 // Create the keyboard (list of keyboard rows)
@@ -127,17 +221,22 @@ public class Telegram extends TelegramLongPollingBot {
                 // Add it to the message
                 keyboardMarkup.setSelective(true);
                 keyboardMarkup.setOneTimeKeyboard(true);
+                keyboardMarkup.setOneTimeKeyboard(true);
+
                 sendMessage.setReplyMarkup(keyboardMarkup);
                 execute(sendMessage);
                 return;
             }
 
             if (stepEnums == StepEnums.MAIN) {
+                SendMessage sendMessage = new SendMessage();
                 sendMessage.setChatId(message.getChatId());
                 execute(registerUserService.branchButton(message.getChatId()));
             }
         } catch (TelegramApiException e) {
             log.warn("TelegramApiException ex {}", e.getMessage());
+        } catch (FileNotFoundException e) {
+            log.warn("FileNotFoundException handle exception {}", e.getMessage());
         }
     }
 
